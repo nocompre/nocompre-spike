11102da61b2f9c468327bd62be880857189431c8
boynextdoor6 <44022858+boynextdoor6@users.noreply.github.com>
2021-04-17 21:02:51 -0700

    a good stop at reg front to back working

---
+++ nc-portal/src/components/Register.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>Registration</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email" />
+    <br>
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password" />
+    <br>
+    <br>
+    <button @click="register">Submit</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
